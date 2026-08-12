f3ef7a210651476430be48442b6a9def3e0230ba
Sivasankar8075 <sivasankarka697@gmail.com>
2026-08-12 12:25:34 +0530

    Clean up duplicate initCursor and stabilize main.js

--- js/main.js
+++ js/main.js
@@ -17,9 +17,6 @@ import { initMusic } from "./music.js";
 import { initSparkles } from "./sparkles.js";
 import { startPhotoRain } from "./photoRain.js";
 
-// DOMContentLoaded ഫങ്ഷനുള്ളിൽ ചേർക്കുക:
-initCursor();
-
 window.addEventListener("DOMContentLoaded", () => {
     console.clear();
     console.log("❤️ Project Riya Main Engine Started");
@@ -33,7 +30,6 @@ window.addEventListener("DOMContentLoaded", () => {
     initLetter();
     initMemory();
     initGallery();
-    // DOMContentLoaded ഫങ്ഷനുള്ളിൽ ചേർക്കുക:
     initCursor();
     initEasterEggs();
     initMusic();
